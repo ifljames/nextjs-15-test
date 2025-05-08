5b702d4f7a77154993eae25bcaf146601e47def9
James <ifljames>
2025-05-08 16:48:21 +0800

    added posts order desc

--- app/actions.ts
+++ app/actions.ts
@@ -57,7 +57,7 @@ export async function getUserBlogPosts(userId: string) {
     );
   }
   const sql = neon(process.env.DATABASE_URL);
-  const data = await sql`SELECT * FROM "BlogPost" WHERE "authorId" = ${userId}`;
+  const data = await sql`SELECT * FROM "BlogPost" WHERE "authorId" = ${userId} ORDER BY "createdAt" DESC`;
   return data || null;
 }
 
@@ -68,6 +68,6 @@ export async function getBlogPost(postId: string) {
     );
   }
   const sql = neon(process.env.DATABASE_URL);
-  const data = await sql`SELECT * FROM "BlogPost" WHERE "id" = ${postId}`;
+  const data = await sql`SELECT * FROM "BlogPost" WHERE "id" = ${postId} ORDER BY "createdAt" DESC`;
   return data[0] || null;
 }
