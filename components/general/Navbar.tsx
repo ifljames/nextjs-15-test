52b4ad0256e75d860f80fba78642cd15492c92c2
James <ifljames>
2025-05-08 16:20:55 +0800

    getting session from client

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 import React from "react";
 import { buttonVariants } from "../ui/button";
@@ -6,11 +8,11 @@ import {
   LoginLink,
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
-const Navbar = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+const Navbar = () => {
+  const { getUser } = useKindeBrowserClient();
+  const user = getUser();
 
   return (
     <nav className="py-5 flex items-center justify-between">
